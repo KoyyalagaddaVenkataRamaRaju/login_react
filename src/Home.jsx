18cc869c6ae3d1c4c32ceb536a348f4c32bde3a7
Koyyalagadda Venkata Rama Raju <ramarajukoyyalagadda@gmail.com>
2024-07-11 18:40:40 +0530

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+
 
 function Home(){
+    axios.defaults.withCredentials=true;
+    useEffect(()=>{
+        axios.get('http://localhost:3001/home')
+        .then(result =>{console.log(result)
+            if(result.data !== "Success"){
+                // navigate('/login')
+            }
+        })
+        .catch(err => console.log(err))
+    })
     return(
         <h1>Home Component</h1>
     )
