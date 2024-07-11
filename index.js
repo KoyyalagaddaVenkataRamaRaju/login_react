229627a177b0d41cd0b4eebb98228031c362aea0
Koyyalagadda Venkata Rama Raju <ramarajukoyyalagadda@gmail.com>
2024-07-11 18:42:01 +0530

    connection auth

--- index.js
+++ index.js
@@ -2,23 +2,51 @@ const express=require("express")
 const mongoose=require('mongoose')
 const cors=require("cors")
 const UserModel=require('./models/User')
+const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
+const cookieParser=require('cookie-parser')
 
 const app=express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:["http://localhost:5173"],
+    methods:["GET","POST"],
+    credentials:true
+}))
+app.use(cookieParser())
 
 mongoose.connect("mongodb://127.0.0.1:27017/user");
+
+const verifyUser=(req,res,next)=>{
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json("The token was not available")
+    }else{
+        jwt.verify(token,"jwt-secret-key",(err,decoded) =>{
+            if (err) return res.json("Token is wrong")
+            next();
+        })
+    }
+}
+
+app.get('/home',verifyUser,(req,res)=>{
+    return res.json("Success")
+})
 
 app.post('/login',(req,res)=>{
     const {email,password}=req.body;
     UserModel.findOne({email:email})
     .then(user =>{
         if(user){
-            if(user.password===password){
-                res.json("Success")
-            }else{
-                res.json("the password is incorrect")
-            }
+            bcrypt.compare(password,user.password,(err,response)=>{
+                if (response) {
+                    const token=jwt.sign({email:user.email},"jwt-secret-key",{expiresIn:"1d"})
+                    res.cookie("token",token);
+                    res.json("Success")
+                }else{
+                    return res.json("the password is incorrect")
+                }
+            })
         }else{
             res.json("No record existed")
         }
@@ -26,9 +54,14 @@ app.post('/login',(req,res)=>{
 })
 
 app.post('/register',(req,res)=>{
-    UserModel.create(req.body)
-    .then(users => res.json(users))
-    .catch(err => res.json(err))
+    const{name,email,password}=req.body;
+    bcrypt.hash(password,10)
+    .then(hash=>{
+        UserModel.create({name,email,password:hash})
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+    }).catch(err=>console.log(err.message))
+
 })
 
 app.listen(3001,()=>{
