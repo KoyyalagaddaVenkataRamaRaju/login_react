426b20bc5811bbed82cce74ce87fd0190fa37d78
Koyyalagadda Venkata Rama Raju <ramarajukoyyalagadda@gmail.com>
2024-07-12 18:48:25 +0530

    commit

--- src/ForgotPassword.jsx
+++ src/ForgotPassword.jsx
@@ -4,19 +4,18 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-function Login() {
+function ForgotPassword() {
     const [email,setEmail]=useState()
-    const [password,setPassword]=useState()
     const navigate=useNavigate()
 
     axios.defaults.withCredentials=true;
     const handleSubmit=(e)=>{
         e.preventDefault()
-        axios.post('http://localhost:3001/login', {email, password})
+        axios.post('http://localhost:3001/forgot-password', {email})
         .then(result => {
           console.log(result)
           if (result.data==="Success"){
-            navigate('/home')
+            navigate('/login')
           }
           
         })
@@ -25,7 +24,7 @@ function Login() {
     return(
       <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
       <div className="bg-white p-3 rounded w-25">
-          <h2>Login</h2>
+          <h4>Forgot Password</h4>
           <form onSubmit={handleSubmit}>
               <div className="mb-3">
                   <label htmlFor="email">
@@ -39,32 +38,13 @@ function Login() {
                   onChange={(e)=>setEmail(e.target.value)}
                    />
               </div>
-              <div className="mb-3">
-                  <label htmlFor="email">
-                      <strong>Password</strong>
-                  </label>
-                  <input type="password"
-                  placeholder="Enter Password"
-                  name="password"
-                  className="form-control rounded-0"
-                  onChange={(e)=>setPassword(e.target.value)}
-                   />
-              </div>
               <button type="submit" className="btn btn-success w-100 rounded-0">
-                  Login
+                  Send
               </button>
               </form>
-          <p>Didn't Have an Account
-            <br />
-            <Link to='/forgot-password'>Forgot Password</Link>
-          </p>
-          <Link to="/register" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
-              Sign Up
-          </Link>
-         
       </div>
   </div>
     )
 }
 
-export default Login;
+export default ForgotPassword;
